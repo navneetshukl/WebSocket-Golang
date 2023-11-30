05d31895b09a70939abe2ed6d1a96282f5064c6f
Navneet shukla <navneetshukla824@gmail.com>
2023-11-30 22:32:52 +0530

    Written method to breadcast message to every connected user

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -3,10 +3,15 @@ package main
 import (
 	"log"
 	"net/http"
+
+	"github.com/navneetshukl/websocket/internal/handlers"
 )
 
 func main() {
 	mux := routes()
+
+	log.Println("Starting channel Listener")
+	go handlers.ListenToWsChannel()
 
 	log.Println("Starting web server on port 8080")
 	_=http.ListenAndServe(":8080",mux)
--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -1,12 +1,16 @@
 package handlers
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 
 	"github.com/CloudyKit/jet/v6"
 	"github.com/gorilla/websocket"
 )
+
+var wsChan=make(chan WsPayload)
+var clients=make(map[WebSocketConnection]string)
 
 var views = jet.NewSet(
 
@@ -60,12 +64,65 @@ func WsEndPoint(w http.ResponseWriter, r *http.Request) {
 	var response WsJsonResponse
 
 	response.Message = `<em><small>Connected to server</small></em>`
+
+	conn:=WebSocketConnection{Conn: ws}
 	err = ws.WriteJSON(response)
+
+	clients[conn]=""
 
 	if err != nil {
 		log.Println(err)
 	}
+
+	go ListenForWs(&conn)
 }
+
+func ListenForWs(conn *WebSocketConnection){
+	defer func(){
+		if r:=recover(); r!=nil{
+			log.Println("Error",fmt.Sprintf("%v",r))
+		}
+	}()
+
+	var payload WsPayload
+
+	for {
+		err:=conn.ReadJSON(&payload)
+
+		if err!=nil{
+
+		} else{
+			payload.Conn=*conn
+			wsChan <- payload
+		}
+	}
+}
+
+func ListenToWsChannel(){
+	var response WsJsonResponse
+
+	for{
+		e:= <-wsChan
+
+		response.Action="Got here"
+		response.Message=fmt.Sprintf("Some message, and action was %s", e.Action)
+		broadcastToAll(response)
+	}
+}
+
+func broadcastToAll(response WsJsonResponse){
+	for client:=range clients{
+		err:=client.WriteJSON(response)
+
+		if err!=nil{
+			log.Println("websocket err")
+			_=client.Close()
+
+			delete(clients,client)
+		}
+	}
+}
+
 func renderPage(w http.ResponseWriter, tmpl string, data jet.VarMap) error {
 	view, err := views.GetTemplate(tmpl)
 	if err != nil {
